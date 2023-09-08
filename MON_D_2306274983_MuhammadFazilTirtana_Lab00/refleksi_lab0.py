3c8d11a8ce93c37981e6a958220d796757258b27
Fazil <faziltirtana@tutanota.com>
2023-09-08 16:22:46 +0700

    Lab00

---
+++ MON_D_2306274983_MuhammadFazilTirtana_Lab00/refleksi_lab0.py
@@ -0,0 +1,17 @@
+# Cetak judul program ini
+print("Perkenalan Diri")
+print("----------------------------------------")
+
+# Melakukan input untuk info yang digunakan dalam perkenalan
+nama_lengkap = input("Nama Lengkap: ")
+nama_panggilan = input("Nama Panggilan: ")
+jurusan = input("Jurusan (Ilmu Komputer / Sistem Informasi): ")
+tempat_tanggal_lahir = input("Tempat dan Tanggal Lahir: ")
+domisili = input("Domisili: ")
+asal_sma = input("Asal SMA: ")
+hobi = input("Hobi: ")
+ekspektasi = input("Ekspektasi dari kelas DDP-1 ini: ")
+
+# Mencetak ucapan perkenalan sesuai dengan input
+print("----------------------------------------")
+print(f"Halo, perkenalkan nama lengkap saya {nama_lengkap}, biasa dipanggil {nama_panggilan}. Saya adalah mahasiswa di Fasilkom jurusan {jurusan}. Saya lahir di {tempat_tanggal_lahir}. Domisili saya di {domisili}. Saya merupakan alumni dari {asal_sma}. Hobi saya yaitu {hobi}. Ekspektasi saya di kelas DDP-1 ini adalah {ekspektasi}")
